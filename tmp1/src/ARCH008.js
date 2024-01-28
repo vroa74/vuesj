5000e8b3d9fdf8e7f03d98f7f665c4b4a37aa5cc
vroa74 <33233830+vroa74@users.noreply.github.com>
2024-01-27 20:23:57 -0600

    ya se importo axios

--- tmp1/src/ARCH008.js
+++ tmp1/src/ARCH008.js
@@ -3,3 +3,5 @@ console.log('-------------------------')
 // API: WqP2DoP9sc18W7E4N8BSsksHYBZlYTho
 // API: UymUs9O4HGiTZIEN6CEkJWUOECA4PAvM
 //  WqP2DoP9sc18W7E4N8BSsksHYBZlYTho
+import axios from 'axios'
+
