90eb0f47ed802646584c089392e0550edc6437e9
vroa74 <33233830+vroa74@users.noreply.github.com>
2024-01-27 20:55:01 -0600

    axios Ruta Completa

--- tmp1/src/ARCH008.js
+++ tmp1/src/ARCH008.js
@@ -1,7 +1,23 @@
 console.log('axios leccion 22');
 console.log('-------------------------')
-// API: WqP2DoP9sc18W7E4N8BSsksHYBZlYTho
-// API: UymUs9O4HGiTZIEN6CEkJWUOECA4PAvM
-//  WqP2DoP9sc18W7E4N8BSsksHYBZlYTho
+// import axios from '../node_modules/axios/dist/axios.js'
 import axios from 'axios'
+
+//  WqP2DoP9sc18W7E4N8BSsksHYBZlYTho
+const apiKey1 ='WqP2DoP9sc18W7E4N8BSsksHYBZlYTho'
+const apiKey2 ='UymUs9O4HGiTZIEN6CEkJWUOECA4PAvM'
+const http= `https://api.giphy.com/v1/gifs/random?api_key=${apiKey1}`
+
+//instacia de  axios previamente configurada
+const giphyApi=axios.create({
+    baseurl:'https://api.giphy.com/v1/gifs',
+    params:{
+        api_key: apiKey1
+
+    }
+})
+
+giphyApi.get('/random')
+    .then(console.log)
+
 
